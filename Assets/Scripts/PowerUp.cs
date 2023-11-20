cd6d24a55c3b9d071f5d0ae0b22e28964195d4ac
Adam <trentowski.adam@gmail.com>
2023-11-20 12:54:45 +0100

    Add PowerUp script with initial logic for powerups

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public enum Type
+    {
+        Coin,
+        ExtraLife,
+        MagicMushroom,
+        Starpower,
+    }
+
+    public Type type;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) {
+            Collect(other.gameObject);
+        }
+    }
+
+    private void Collect(GameObject player)
+    {
+        switch (type)
+        {
+            case Type.Coin:
+                GameManager.Instance.AddCoin();
+                break;
+            case Type.ExtraLife:
+                GameManager.Instance.AddLife();
+                break;
+            case Type.MagicMushroom:
+                // TODO
+                break;
+            case Type.Starpower:
+                // TODO
+                break;
+        }
+
+        Destroy(gameObject);
+    }
+
+}
