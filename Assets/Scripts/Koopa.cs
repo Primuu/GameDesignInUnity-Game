73d349257b69d728638a4b11259473ac6b0c6566
Adam <trentowski.adam@gmail.com>
2023-11-20 09:55:32 +0100

    Update Koopa logic, add pushing shell

--- Assets/Scripts/Koopa.cs
+++ Assets/Scripts/Koopa.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class Koopa : MonoBehaviour
 {
     public Sprite shellSprite;
+    public float shellSpeed = 12f;
 
     private bool shelled;
-    private bool shellMoving;
+    private bool pushed;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -20,13 +21,38 @@ public class Koopa : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (shelled && other.CompareTag("Player")) {
+            if (!pushed) {
+                Vector2 direction = new Vector2(transform.position.x - other.transform.position.x, 0f);
+                PushShell(direction);
+            } else {
+                Player player = other.GetComponent<Player>();
+                player.Hit();
+            }
+        }
+    }
+
     private void EnterShell()
     {
         shelled = true;
-        
+
         GetComponent<EntityMovement>().enabled = false;
         GetComponent<AnimatedSprite>().enabled = false;
         GetComponent<SpriteRenderer>().sprite = shellSprite;
     }
 
+    private void PushShell(Vector2 direction)
+    {
+        pushed = true;
+
+        GetComponent<Rigidbody2D>().isKinematic = false;
+
+        EntityMovement movement = GetComponent<EntityMovement>();
+        movement.direction = direction.normalized;
+        movement.speed = shellSpeed;
+        movement.enabled = true;
+    }
+    
 }
