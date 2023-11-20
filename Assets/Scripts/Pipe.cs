c5c8270175a2f05ac691ca484c67a98e114cd504
Adam <trentowski.adam@gmail.com>
2023-11-20 14:13:37 +0100

    Add prototype Pipe script

---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    public Transform connection;
+    public KeyCode enterKeyCode = KeyCode.S;
+    public Vector3 enterDirection = Vector3.down;
+    public Vector3 exitDirection = Vector3.zero;
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (connection != null && other.CompareTag("Player")) {
+            if (Input.GetKey(enterKeyCode)) {
+                StartCoroutine(Enter(other.transform));
+            }
+        }
+    }
+
+    private IEnumerator Enter(Transform player)
+    {
+        player.GetComponent<PlayerMovement>().enabled = false;
+
+        Vector3 enteredPosition = transform.position + enterDirection;
+        Vector3 enteredScale = Vector3.one * 0.5f;
+
+        yield return Move(player, enteredPosition, enteredScale);
+    }
+
+    private IEnumerator Move(Transform player, Vector3 endPosition, Vector3 endScale)
+    {
+        float elapsed = 0f;
+        float duration = 1f;
+
+        Vector3 startPosition = player.position;
+        Vector3 startScale = player.localScale;
+
+        while (elapsed < duration)
+        {
+            float animationPercentage = elapsed / duration;
+
+            player.position = Vector3.Lerp(startPosition, endPosition, animationPercentage);
+            player.localScale = Vector3.Lerp(startScale, endScale, animationPercentage);
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        player.position = endPosition;
+        player.localScale = endScale;
+    }
+
+}
