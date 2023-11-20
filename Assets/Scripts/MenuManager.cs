ffe560502ffdb10235a1f48f0f3a453247f1d9c9
Adam <trentowski.adam@gmail.com>
2023-11-20 22:20:21 +0100

    Add complete menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
+using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
+    private bool play = false;
+    public TextMeshProUGUI playButtonText;
     public GameObject menuPanel;
+    public GameObject restartButton;
 
     private void Start()
     {
-        menuPanel.SetActive(false);
+        Time.timeScale = 0f;
+        playButtonText.text = "PLAY";
+        menuPanel.SetActive(true);
+        
+        restartButton.SetActive(false);
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (play && Input.GetKeyDown(KeyCode.Escape))
         {
             ToggleMenu();
         }
@@ -20,6 +28,7 @@ public class MenuManager : MonoBehaviour
     public void ToggleMenu()
     {
         menuPanel.SetActive(!menuPanel.activeSelf);
+        restartButton.SetActive(play);
 
         Time.timeScale = menuPanel.activeSelf ? 0f : 1f;
     }
@@ -28,6 +37,27 @@ public class MenuManager : MonoBehaviour
     {
         menuPanel.SetActive(false);
         Time.timeScale = 1f;
+        
+        play = true;
+        playButtonText.text = "RESUME";
+        restartButton.SetActive(play);
+    }
+
+    public void RestartGame()
+    {
+        GameManager.Instance.LoadLevel(1, 1, true);
+
+        menuPanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
     }
 
 }
