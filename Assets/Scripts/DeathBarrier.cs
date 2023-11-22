f10e97cb555e232d730376cad64d67c95960f239
Adam <trentowski.adam@gmail.com>
2023-11-22 22:08:24 +0100

    Add player death sound after fall

--- Assets/Scripts/DeathBarrier.cs
+++ Assets/Scripts/DeathBarrier.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class DeathBarrier : MonoBehaviour
 {
+    public AudioClip deathSound;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player")) {
             other.gameObject.SetActive(false);
             AudioManager.Instance.StopSFX();
+            AudioManager.Instance.StopMusic();
+            AudioManager.Instance.PlaySFX(deathSound);
             GameManager.Instance.ResetLevel(3f);
         } else {
             Destroy(other.gameObject);
